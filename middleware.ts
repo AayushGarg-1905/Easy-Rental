385ce128aaff0ba3093c509622bba342174a0eaa
Aayush Garg <masteraayushgarg@gmail.com>
2025-01-01 19:49:35 +0530

    Changes in middleware

--- middleware.ts
+++ middleware.ts
@@ -12,16 +12,12 @@ import { NextResponse } from 'next/server';
 const isPublicRoute = createRouteMatcher(['/', '/properties(.*)']);
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
-  
+export default clerkMiddleware((auth, req) => {
   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
   if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL('/', req.url));
   }
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
-  return NextResponse.next();
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
