2b42ddf867755f07dcebc628d9337e5e76f05cff
Aayush Garg <masteraayushgarg@gmail.com>
2025-06-12 16:49:35 +0530

    Updated config for supabase storage images

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             },
             {
                 protocol:'https',
-                hostname:'kljfhcbktqytnmykixfb.supabase.co'
+                hostname:'xgxdxmhjwguzcqiaowin.supabase.co'
             }
         ]
     }
